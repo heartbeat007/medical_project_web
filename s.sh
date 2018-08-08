33555fa681ee94fbb67c697daded51538b7b5588
root <root@localhost.localdomain>
2018-08-07 21:25:14 -0400

    Update s.sh

---
+++ s.sh
@@ -0,0 +1,6 @@
+echo "# medical_project_web" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/heartbeat007/medical_project_web.git
+git push -u origin master
